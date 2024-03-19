fdef8739d1c6faac246551d2e7beb2d130c67356
Yura <holod1426@gmail.com>
2024-03-19 06:51:29 +0300

    2024-03-19 06:45

--- internal/service/copter_service.go
+++ internal/service/copter_service.go
@@ -25,48 +25,33 @@ type ModeProperties struct {
 
 func (S CopterService) GetProperties() (copter.CopterResponse, error) {
 
-	// Навесное оборудование
-	var attachments = S.Props.AttachmentsProperties
-	// ESC
-	var esc = S.Props.ControllerProperties
-	// Внешняя среда
-	var environment = S.Props.EnvironmentProperties
-	// Мотор
-	var motor = S.Props.MotorProperties
-	// Рама
-	var frame = S.Props.FrameProperties
-	// Пропеллер
-	var propeller = S.Props.PropellerProperties
-	// Аккумулятор
-	var battery = S.Props.BatteryProperties
-
 	// Ищем композит аккумулятора с ВАХ
 	var composit models.Composit
-	if err := postgres.DB.Where("id = ?", battery.CompositId).First(&composit).Error; err != nil {
+	if err := postgres.DB.Where("id = ?", S.Props.BatteryProperties.CompositId).First(&composit).Error; err != nil {
 		exeptions.InternalServerError(S.C, err)
 		return copter.CopterResponse{}, err
 	}
 
 	// Вычисляем параметры окружающей среды
-	envProps, envWarn := EnvironmentService{}.GetProperties(environment)
+	envProps, envWarn := EnvironmentService{}.GetProperties(S.Props.EnvironmentProperties)
 
 	// Вычисляем параметры батареи
-	battProps, err := BatteryService{}.GetProperties(battery, composit)
+	battProps, err := BatteryService{}.GetProperties(S.Props.BatteryProperties, composit)
 	if err != nil {
 		exeptions.InternalServerError(S.C, err)
 		return copter.CopterResponse{}, err
 	}
 	// Вычисляем параметры ESC
-	escWarn := ControllerService{}.GetProperties(esc, battProps.BatteryVoltage)
+	escWarn := ControllerService{}.GetProperties(S.Props.ControllerProperties, battProps.BatteryVoltage)
 
 	// Вычисляем параметры пропеллера
-	propProps, propWarn := PropellerService{}.GetProperties(propeller, frame)
+	propProps, propWarn := PropellerService{}.GetProperties(S.Props.PropellerProperties, S.Props.FrameProperties)
 
 	// Вычисляем параметры мотора
-	motorProps, motorWarn := MotorService{}.GetProperties(motor, frame)
+	motorProps, motorWarn := MotorService{}.GetProperties(S.Props.MotorProperties, S.Props.FrameProperties)
 
 	// Вычисление общих параметров для обоих режимов полета
-	generalProps := GeneralService{}.GetProperties(frame, attachments, battProps.Mass, esc.Mass, propProps.Mass, motorProps.Mass)
+	generalProps := GeneralService{}.GetProperties(S.Props.FrameProperties, S.Props.AttachmentsProperties, battProps.Mass, S.Props.ControllerProperties.Mass, propProps.Mass, motorProps.Mass)
 
 	// Вычисляем параметры для режима зависания
 	hoverProps, hoverWarn := ModeProperties{Props: S.Props, Calc: copter.StandartProperties{
